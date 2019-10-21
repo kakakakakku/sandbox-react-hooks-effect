38f579e78cd981d107a994eec1092caae4041d24
kakakakakku <y.yoshida22@gmail.com>
2019-10-22 01:29:14 +0900

    Added API Call

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App: React.FC = () => {
   const [count, setCount] = useState(0);
@@ -8,12 +9,24 @@ const App: React.FC = () => {
     return (() => console.log('Clean Up 🐤'));
   }, [count]);
 
+  const [meowUrl, setMeowUrl] = useState('');
+
+  useEffect(() => {
+    const fetch = async () => {
+      const results = await axios('http://aws.random.cat/meow');
+      setMeowUrl(results.data.file)
+    };
+    fetch();
+  }, []);
+
   return (
     <div>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>
         Click me
       </button>
+      <hr />
+      <img src={meowUrl} style={{ height: 200 }} />
     </div>
   );
 }
